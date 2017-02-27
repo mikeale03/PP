91e60fcaafd7f51beacac077c3e9429bc16503a2
mikeale03 <mikeale03@gmail.com>
2017-02-28 00:34:17 +0800

    ad

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ mongoose.connect('mongodb://fcc-pp:codepair@ds151008.mlab.com:51008/mongotest03'
 
 
 
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
+//app.set("view engine", "ejs");
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.get('/', function(req, res){
+  console.log('home has been hit');
+    res.sendFile( __dirname + '/build/index.html');
+});
 let itemRoute = require('./item-route');
 app.use('/items',itemRoute);
 app.listen(4000, () => {
